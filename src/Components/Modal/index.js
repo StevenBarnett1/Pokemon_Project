66f19b405d130382c42bcf27e1e4f8b060493350
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-12-07 13:41:05 -0800

    styled modal a bit

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -22,12 +22,11 @@ function PokemonModal(props) {
     <>
       {modalView && (
         <Modal onClose={() => handleOnModalClose()}>
-          <div className = "modal-item"><div>Name:</div><div> {modalInfo && modalInfo.name}</div></div>
-          <div className = "modal-item"><div>Species:</div><div> {modalInfo && modalInfo.stats.species.name}</div></div>
-          <div className = "modal-item"><div>Types:</div><div> {modalInfo && modalInfo.stats.types.map(current => (<div key = {current.type.name}>{current.type.name}</div>))}</div></div>
-          <div className = "modal-item"><div>Height:</div><div> {modalInfo && modalInfo.stats.height}</div></div>
-          <div className = "modal-item"><div>Weight:</div><div> {modalInfo && modalInfo.stats.weight}</div></div>
-          <div className = "modal-item"><div>Moves:</div> <div id = "moves-list">{modalInfo && modalInfo.stats.moves.map(current => (<div key = {current.move.name}>{current.move.name}</div>))}</div></div>
+          <div id = "name" className = "modal-item">{modalInfo && modalInfo.name}</div>
+          <div className = "modal-item"><div>Types:</div><div className = "mapped-types item-result"> {modalInfo && modalInfo.stats.types.map((current,i) => (<div key = {current.type.name}>{current.type.name}{i !== modalInfo.stats.types.length-1 ? "/" : ""}</div>))}</div></div>
+          <div className = "modal-item"><div>Height:</div> <div className = "item-result"> {modalInfo && modalInfo.stats.height}</div></div>
+          <div className = "modal-item"><div>Weight:</div> <div className = "item-result"> {modalInfo && modalInfo.stats.weight}</div></div>
+          <div id = "move-list" className = "modal-item"><div id = "move-list-title">Move List</div> <div id = "moves-list">{modalInfo && modalInfo.stats.moves.map(current => (<div key = {current.move.name}>{current.move.name}</div>))}</div></div>
         </Modal>
       )}
     </>
