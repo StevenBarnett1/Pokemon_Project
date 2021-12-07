d0cb8aeb779e48455f774334dd25de078f484be1
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-12-07 14:41:21 -0800

    entire project done, modal doesnt look very goood but the functionality and basic positional styling is there

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -22,11 +22,20 @@ function PokemonModal(props) {
     <>
       {modalView && (
         <Modal onClose={() => handleOnModalClose()}>
-          <div id = "name" className = "modal-item">{modalInfo && modalInfo.name}</div>
-          <div className = "modal-item"><div>Types:</div><div className = "mapped-types item-result"> {modalInfo && modalInfo.stats.types.map((current,i) => (<div key = {current.type.name}>{current.type.name}{i !== modalInfo.stats.types.length-1 ? "/" : ""}</div>))}</div></div>
-          <div className = "modal-item"><div>Height:</div> <div className = "item-result"> {modalInfo && modalInfo.stats.height}</div></div>
-          <div className = "modal-item"><div>Weight:</div> <div className = "item-result"> {modalInfo && modalInfo.stats.weight}</div></div>
-          <div id = "move-list" className = "modal-item"><div id = "move-list-title">Move List</div> <div id = "moves-list">{modalInfo && modalInfo.stats.moves.map(current => (<div key = {current.move.name}>{current.move.name}</div>))}</div></div>
+          <div id = "name" className = "modal-item">{modalInfo && modalInfo.name[0].toUpperCase() + modalInfo.name.slice(1)}</div>
+          <div id = "attributes">
+            <div id = "left">
+                <div className = "modal-item-label">Types:</div>
+                <div className = "modal-item-label">Height:</div>
+                <div className = "modal-item-label">Weight:</div>
+            </div>
+            <div id = "right">
+                <div className = "mapped-types item-result"> {modalInfo && modalInfo.stats.types.map((current,i) => (<div key = {current.type.name}>{current.type.name}{i !== modalInfo.stats.types.length-1 ? "/" : ""}</div>))}</div>
+                <div className = "item-result"> {modalInfo && modalInfo.stats.height}</div>
+                <div className = "item-result"> {modalInfo && modalInfo.stats.weight}</div>
+            </div>
+          </div>
+          <div id = "move-list" className = "modal-item"><div id = "move-list-title" className = "modal-item-label">Move List</div> <div id = "moves-list">{modalInfo && modalInfo.stats.moves.map(current => (<div key = {current.move.name}>{current.move.name}</div>))}</div></div>
         </Modal>
       )}
     </>
